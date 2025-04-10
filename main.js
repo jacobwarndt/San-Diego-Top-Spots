0438dbd2a92ee45a8e91f429ab89684d58685689
JacobArndt <jacobwarndt@gmail.com>
2025-04-10 19:59:58 -0400

    Implement data fetching and rendering for top spots from JSON

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-// write your code here
-
+    $.getJSON('data.json', function(data) {
+        data.forEach(function(spot) {
+            const row =
+                `<tr>
+                    <td>${spot.name}</td>
+                    <td>${spot.description}</td>
+                    <td>
+                        <a href="https://www.google.com/maps?q=${spot.location[0]},${spot.location[1]}" target="_blank">Open in Google Maps</a>
+                    </td>
+                </tr>`;
+            $('#top-spots').append(row);
+        });
+    });
 });
- 
